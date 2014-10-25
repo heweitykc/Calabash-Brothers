c2d8aa0a425818096408e3928e80aa9db94fbde5
callee <heweitykc@163.com>
2014-10-25 08:42:47 +0800

    111

---
+++ u3dgamelibs/gamenetlib/NetDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gamenetlib
+{
+    public class NetDemo
+    {
+        static public void doit()
+        {
+            Console.WriteLine("我要痴迷万分。。。。");
+        }
+    }
+}
